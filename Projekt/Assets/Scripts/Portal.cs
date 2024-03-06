52a9064d5255f3793865e2d0f5e9b1c25fc77931
KacperBar99 <kacperbar99@gmail.com>
2024-03-06 13:34:32 +0100

    Baked Lightning and smart camera

--- Projekt/Assets/Scripts/Portal.cs
+++ Projekt/Assets/Scripts/Portal.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PortalTeleport : MonoBehaviour
+public class Portal : MonoBehaviour
 {
     private Transform player;
     public Transform receiver;
-    [SerializeField,Tooltip("Camera to adjust")]
 
-    
+    [SerializeField, Tooltip("Portal Camera")]
+    private Camera cameraToDisable;
+    private Camera mainCamera;
+    private Plane[] cameraFrustum;
+    private Bounds bounds;
 
 
     private bool playerIsOverlapping = false;
@@ -17,15 +20,42 @@ public class PortalTeleport : MonoBehaviour
     {
         this.player = GameObject.FindGameObjectWithTag("Player").transform;
     }
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        bounds = GetComponent<Collider>().bounds;
+        cameraToDisable.enabled = false;
+    }
 
 
     private void LateUpdate()
     {
+        Vector3 portalToPlayer = player.position - transform.position;
+        float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+        cameraFrustum = GeometryUtility.CalculateFrustumPlanes(mainCamera);
+
+        if (dotProduct<0)
+        {
+            if (cameraToDisable.enabled)
+            {
+                cameraToDisable.enabled = false;
+            }
+        }
+        else
+        {
+            if (!cameraToDisable.enabled && GeometryUtility.TestPlanesAABB(cameraFrustum, bounds))
+            {
+                cameraToDisable.enabled = true;
+            }
+            else if (cameraToDisable.enabled && !GeometryUtility.TestPlanesAABB(cameraFrustum, bounds))
+            {
+                cameraToDisable.enabled = false;
+            }
+        }
+
         if (this.playerIsOverlapping)
         {
-            Vector3 portalToPlayer = player.position - transform.position;
-            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
-
             if(dotProduct < 0f)
             {
                 float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
