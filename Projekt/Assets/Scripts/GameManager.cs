83c9788a7cf00be7009c87344110c965d1663678
KacperBar99 <80577315+KacperBar99@users.noreply.github.com>
2024-05-12 14:48:08 +0200

    update

--- Projekt/Assets/Scripts/GameManager.cs
+++ Projekt/Assets/Scripts/GameManager.cs
@@ -9,6 +9,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        UnityEditor.EditorPrefs.SetBool("DeveloperMode", false);
         if (maxFPS)
         {
             Application.targetFrameRate = -1;
